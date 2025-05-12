44f7658770c3bd39cc5f88f49bbf4d36bfdbebe1
root <root@fongcon.fongcon.com>
2025-05-12 14:01:25 +0800

    update k9s install

---
+++ k8s_tool.sh
@@ -0,0 +1,5 @@
+# k9s
+wget https://github.com/derailed/k9s/releases/download/v0.50.6/k9s_Linux_amd64.tar.gz
+
+# helm3
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
